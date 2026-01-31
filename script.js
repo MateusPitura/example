b36389c64886ddc7a564c4065ac3fcd2732add00
MateusPitura <mateuspitura@gmail.com>
2026-01-31 18:06:56 -0300

    Update version to 4 in HTML and JS

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 window.addEventListener('DOMContentLoaded', () => {
     const versionElement = document.getElementById('version');
-    versionElement.textContent = "3";
+    versionElement.textContent = "4";
 });
